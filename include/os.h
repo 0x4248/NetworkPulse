7359575eb81ed7440ebb4b3c794049c8edf338a5
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-01-14 09:52:24 +0000

    Add comments to OS
    Signed-off-by: lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>

--- include/os.h
+++ include/os.h
@@ -1,3 +1,13 @@
+/**
+ * Network Pulse
+ * A simple arp table like tool to see if clients are connected and alive on the network.
+ * GitHub: https://www.github.com/lewisevans2007/NetworkPulse
+ * Licence: GNU General Public License v3.0
+ * By: Lewis Evans
+ * 
+ * OS header file
+*/
+
 #ifdef __linux__
 #define OS "Linux"
 #elif __APPLE__
