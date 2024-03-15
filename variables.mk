24679093c02d2f5ad82f52b62826f6c62cbe750d
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-03-15 18:40:26 +0000

    Move makefile variables to variables.mk
    Signed-off-by: lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>

---
+++ variables.mk
@@ -0,0 +1,15 @@
+# Network Pulse
+# A simple arp table like tool to see if clients are connected and alive on the network.
+# GitHub: https://www.github.com/lewisevans2007/NetworkPulse
+# Licence: GNU General Public License v3.0
+# By: Lewis Evans
+#
+# Makefile variables for the main makefile:
+
+CC = g++
+CFLAGS = -std=c++11 -g
+
+C_CC = gcc
+C_CFLAGS = -Wall -std=c11 -g
+
+BIN = bin
